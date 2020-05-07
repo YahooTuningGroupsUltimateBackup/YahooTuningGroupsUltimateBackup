999afc5f0b12cd9efefc34897aa3276939e5d9ea
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-05-06 21:19:31 -0700

    attempt at frontend search but it crashes cuz there is too much content

--- parser.js
+++ parser.js
@@ -1,8 +1,10 @@
 const fs = require('fs')
+const path = require('path')
 const he = require('he')
 const {simpleParser} = require('mailparser')
 
 const ROOT_PAGE = 'dist/index.html'
+const HOST_SITE = 'https://yahootuninggroupsultimatebackup.github.io'
 
 const ATTACHMENT_STYLE = 'margin: 0px 20px; padding: 20px; background-color: #ddd'
 const EMAIL_TEXT_STYLE = 'margin: 0px 20px 20px; padding: 20px; background-color: #eee'
@@ -41,7 +43,7 @@ const writeListHtml = list => {
     fs.appendFileSync(listPage, `<a href='/'>back to root</a>`)
     fs.appendFileSync(listPage, `<h1>${list}</h1>`)
 
-    Object.entries(parsed[list]).forEach(([listTopicId, elements]) => {
+    parsed[list] && Object.entries(parsed[list]).forEach(([listTopicId, elements]) => {
         const listTopicPageFile = `topicId_${listTopicId}.html`
         const listTopicPage = `dist/${list}/${listTopicPageFile}`
 
@@ -59,6 +61,20 @@ const writeListHtml = list => {
         const topicName = initialElement.email.subject
         fs.appendFileSync(listTopicPage, `<a href="/${list}">back to list</a>`)
         fs.appendFileSync(listTopicPage, `<h1>${topicName}</h1>`)
+
+        let allTopicText = elements.reduce(
+            (textSoFar, el) => {
+                return `${textSoFar} ${he.encode(el.email.from.text)} ${el.email.text}`
+            },
+            '',
+        )
+        const text = `${topicName} ${allTopicText}`
+        const searchObject = {
+            title: topicName,
+            text,
+            url: `${HOST_SITE}/${list}/${listTopicPageFile}`
+        }
+        fs.appendFileSync('dist/tipuesearch/tipuesearch_content.js', `${JSON.stringify(searchObject)},\n`)
 
         fs.appendFileSync(listPage, `
             <div style="border-bottom: 1px solid lightgrey;">
@@ -133,27 +149,78 @@ const parseList = list => {
     })
 }
 
-const setupPage = (list) => {
+const setupPage = (list, topicName) => {
     const header = `<!DOCTYPE html>
         <html>
-        <head>
-        <meta charset="utf-8">
-            <meta name="viewport"
-        content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no">
-            <meta http-equiv="x-ua-compatible" content="ie=edge">
-            <title>Yahoo Tuning Groups Ultimate Backup ${list || 'Root'}</title>
-        </head>
-        <body>
-        </body>
+            <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no">
+                <meta http-equiv="x-ua-compatible" content="ie=edge">
+                <title>Yahoo Tuning Groups Ultimate Backup ${list ? ` - ${list}` : ''} ${topicName ? ` - ${topicName}` : ''}</title>
+                <link rel="stylesheet" href="tipuesearch/tipuesearch.css" type="text/css" >
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+                <script src="tipuesearch/tipuesearch_content.js"></script>
+                <script src="tipuesearch/tipuesearch_set.js"></script>
+                <script src="tipuesearch/tipuesearch.min.js"></script>
+            </head>
+            <body>
+            </body>
         </html>
+        
+        ${list ? '' : `<form>
+            <div class="tipue_search_group">
+                <input type="text" name="q" id="tipue_search_input" pattern=".{3,}" title="At least 3 characters" required>
+                <button type="submit" class="tipue_search_button">
+                    <div class="tipue_search_icon">&#9906;</div>
+                </button>
+            </div>
+        </form>
+        
+        <div id="tipue_search_content"></div>
+        
+        <script>
+            $('#tipue_search_input').tipuesearch()
+        </script>`}
     `
     fs.appendFileSync(`dist/${list ? `${list}/` : '/'}index.html`, header)
+}
+
+const copyFileSync = (source, target) => {
+    let targetFile = target
+
+    if (fs.existsSync(target) && fs.lstatSync(target).isDirectory()) {
+        targetFile = path.join(target, path.basename(source))
+    }
+
+    fs.writeFileSync(targetFile, fs.readFileSync(source))
+}
+
+const copyFolderRecursiveSync = (source, target) => {
+    let targetFolder = path.join(target, path.basename(source))
+    if (!fs.existsSync(targetFolder)) {
+        fs.mkdirSync(targetFolder)
+    }
+
+    if (fs.lstatSync(source).isDirectory()) {
+        fs.readdirSync(source).forEach(file => {
+            const curSource = path.join(source, file)
+            if (fs.lstatSync(curSource).isDirectory()) {
+                copyFolderRecursiveSync(curSource, targetFolder)
+            } else {
+                copyFileSync(curSource, targetFolder)
+            }
+        })
+    }
 }
 
 deleteFolderRecursive('dist')
 fs.mkdirSync('dist')
 
+fs.mkdirSync('dist/tipuesearch')
+copyFolderRecursiveSync('vendor/tipuesearch', 'dist')
+
 setupPage()
+fs.appendFileSync('dist/tipuesearch/tipuesearch_content.js', `var tipuesearch = {"pages": [\n`)
 fs.appendFileSync(ROOT_PAGE, `<h1>Yahoo Tuning Groups Ultimate Backup</h1>`)
 
 fs.readdir(`src`, (err, lists) => lists.forEach(parseList))
